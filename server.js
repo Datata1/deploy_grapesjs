9a87ea8b525a494986bcc354c7ff7db89b1731b4
Jan-David Wiederstein <141321444+Datata1@users.noreply.github.com>
2023-12-19 16:47:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -23,23 +23,106 @@ app.get('/', (req, res) => {
     const htmlFiles = files.filter(file => file.endsWith('.html'));
 
     // Generiere eine Liste von Links zu den HTML-Dateien
-    const pageLinks = htmlFiles.map(file => `<li><a href="/my_webpages/${file}">${file}</a></li>`).join('');
+    const pageLinks = htmlFiles.map(file => `<li><a class="page-button" href="/my_webpages/${file}">${file}</a></li>`).join('');
 
     // Generiere eine einfache HTML-Seite mit Links zu den Webseiten
     const overviewHTML = `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Webpage Overview</title>
-      </head>
-      <body>
-        <h1>Webpage Overview</h1>
-        <ul>${pageLinks}</ul>
-		<a href="/admin">Webpage builder</a>
-      </body>
-      </html>
+		<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>My Webpages Built with GrapesJS</title>
+	  <style>
+		body {
+		  font-family: 'Arial', sans-serif;
+		  margin: 0;
+		  padding: 0;
+		  background-color: #f4f4f4;
+		  color: #333;
+		  display: flex;
+		  flex-direction: column;
+		  align-items: center;
+		  justify-content: center;
+		  height: 100vh;
+		}
+
+		h1 {
+		  color: #0066cc;
+		  font-size: 36px;
+		  margin-top: 0;
+		}
+
+		.pages-box {
+		  background-color: #fff;
+		  border: 1px solid #ddd;
+		  padding: 20px;
+		  border-radius: 8px;
+		  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+		  margin-bottom: 20px;
+		  width: 80%;
+		  max-width: 600px;
+		  display: flex;
+		  flex-wrap: wrap;
+		  gap: 10px; /* Abstand zwischen den Links */
+		  justify-content: center; /* Änderung hier: Zentrieren der Buttons */
+		}
+
+		ul {
+		  list-style-type: none;
+		  padding: 0;
+		  margin: 0;
+		  display: flex;
+		  flex-wrap: wrap;
+		  gap: 10px; /* Abstand zwischen den Links */
+		  justify-content: center; /* Änderung hier: Zentrieren der Buttons */
+		}
+
+		li {
+		  margin-bottom: 10px;
+		}
+
+		.page-button {
+		  display: inline-block;
+		  padding: 10px 20px;
+		  width: 250px; /* Feste Breite für alle Buttons */
+		  font-size: 16px;
+		  background-color: #0066cc;
+		  color: #fff;
+		  text-decoration: none;
+		  border-radius: 5px;
+		  transition: background-color 0.3s ease-in-out;
+		  text-align: center; /* Zentrieren des Textes */
+		}
+
+		.page-button:hover {
+		  background-color: #004080;
+		}
+
+		.button {
+		  display: inline-block;
+		  padding: 15px 30px;
+		  font-size: 18px;
+		  background-color: #0066cc;
+		  color: #fff;
+		  text-decoration: none;
+		  border-radius: 5px;
+		  transition: background-color 0.3s ease-in-out;
+		}
+
+		.button:hover {
+		  background-color: #004080;
+		}
+	  </style>
+	</head>
+	<body>
+	  <h1>My Webpages</h1>
+	  <div class="pages-box">
+		<ul>${pageLinks}</ul>
+	  </div>
+	  <a href="/admin" class="button">GrapesJS - Low-Code Webpage Builder</a>
+	</body>
+	</html>
     `;
 
     res.send(overviewHTML);
